bc95aff273e6c24ce558730a4fddb5b79a5dc1c3
Parham Alvani <parham.alvani@gmail.com>
2017-05-18 15:40:00 +0430

    Clean version of calc.py was added.

--- webappdirectory/calc.py
+++ webappdirectory/calc.py
@@ -1,28 +1,24 @@
-from flask import Flask,render_template,request
-import requests,os
+from flask import Flask, render_template, request
+import requests
+import os
 
 
 app = Flask(__name__)
 
+
 @app.route('/')
-
 def calc():
+    return render_template('index.html')
 
-    return render_template(
 
-        'index.html')
-
-@app.route('/',methods=['POST'])
-
+@app.route('/', methods=['POST'])
 def getnum():
-	text = request.form['textarea']
-	num = text.split()
-	ip = os.environ['add_ip']
-        r = requests.post('http://' + ip + ':3000',json={"param1": num[0],"param2" : num[2]})
-	
+    text = request.form['textarea']
+    num = text.split()
+    ip = os.environ['add_ip']
+    requests.post('http://' + ip + ':3000', json={"param1": num[0],
+                                                  "param2": num[2]})
 
 
 if __name__ == "__main__":
-
-    app.run(host = '0.0.0.0')
-
+    app.run(host='0.0.0.0')
